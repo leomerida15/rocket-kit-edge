1a3559c158c5e8830300bc6e81d8be219ce5ec69
leomerida15 <dimasmerida15@gmail.com>
2024-09-10 12:41:35 -0400

    cors resolver

--- src/onEdge/responseFactory.ts
+++ src/onEdge/responseFactory.ts
@@ -4,23 +4,20 @@ export interface ReplyInit extends Omit<ResponseInit, "status"> {
 	status?: StatusCodes;
 }
 
+// Configurar los encabezados CORS
+const headers = new Headers();
+headers.set("Content-Type", "application/json");
+headers.set("Access-Control-Allow-Origin", "*"); // Puedes restringir esto a un dominio específico, como 'http://localhost:4040'
+headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+headers.set(
+	"Access-Control-Allow-Headers",
+	"authorization, x-client-info, apikey, content-type",
+);
+
 const getReplyInit = (init?: ReplyInit) => {
 	if (init?.status && !init?.statusText) {
 		init.statusText = getReasonPhrase(init.status);
 	}
-
-	const headers = new Headers();
-
-	headers.set("Content-Type", "application/json");
-	headers.set("Access-Control-Allow-Origin", "*");
-	headers.set(
-		"Access-Control-Allow-Methods",
-		"GET, POST, PUT, PATCH, DELETE, OPTIONS",
-	);
-	headers.set(
-		"Access-Control-Allow-Headers",
-		"authorization, x-client-info, apikey, content-type",
-	);
 
 	const InitConfig = {
 		headers: { ...headers, ...init?.headers },
