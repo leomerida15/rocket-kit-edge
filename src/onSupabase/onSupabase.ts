1d16b2f924f574b8cbb5090129f93aa5305f1a09
leomerida15 <dimasmerida15@gmail.com>
2025-08-30 17:13:02 -0400

    v2.0.0

--- src/onSupabase/onSupabase.ts
+++ src/onSupabase/onSupabase.ts
@@ -1,5 +1,4 @@
-import { SupabaseClient, createClient } from "@supabase/supabase-js";
-import { GenericSchema } from "@supabase/supabase-js/dist/module/lib/types";
+import { createClient } from "@supabase/supabase-js";
 import { RocketEnvs } from "../global.env";
 import { EdgeError } from "../index";
 
@@ -8,15 +7,9 @@ const key = process.env.SUPABASE_ANON_KEY;
 
 export const onSupabase = <
 	Database,
-	SchemaName extends string & keyof Database = "public" extends keyof Database
-		? "public"
-		: string & keyof Database,
-	Schema extends GenericSchema = Database[SchemaName] extends GenericSchema
-		? Database[SchemaName]
-		: any,
 >(
 	options?: Parameters<typeof createClient<Database>>[2],
-): SupabaseClient<Database, SchemaName, Schema> => {
+) => {
 	const jwt = RocketEnvs.get("SUPABASE_JWT");
 
 	if (!url || !key || !jwt) throw new EdgeError();
@@ -33,5 +26,5 @@ export const onSupabase = <
 		},
 	});
 
-	return supabase as SupabaseClient<Database, SchemaName, Schema>;
+	return supabase;
 };
